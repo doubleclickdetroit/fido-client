60529504df1b7e545422c3405f267f8e20ecf9e1
Ben Babics <bbabics@gmail.com>
2015-09-07 21:11:34 -0400

    create person ocassions

--- app/pods/contacts/new/route.js
+++ app/pods/contacts/new/route.js
@@ -1,16 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  genders      : Ember.inject.service( 'genders' ),
-  relationships: Ember.inject.service( 'relationships' ),
-  occasions    : Ember.inject.service( 'occasions' ),
+  genders        : Ember.inject.service( 'genders' ),
+  relationships  : Ember.inject.service( 'relationships' ),
+  occasionPresets: Ember.inject.service( 'occasions' ),
 
   model() {
     return Ember.RSVP.hash({
-      user         : this.store.createRecord( 'contact' ),
-      genders      : this.get( 'genders.list' ),
-      relationships: this.get( 'relationships.list' ),
-      occasions    : this.get( 'occasions.list' )
+      user           : this.store.createRecord( 'contact' ),
+      occasions      : this.store.all( 'occasion' ),
+      genders        : this.get( 'genders.list' ),
+      relationships  : this.get( 'relationships.list' ),
+      occasionPresets: this.get( 'occasionPresets.list' )
     });
+  },
+
+  actions: {
+    selectOccasion(occasion) {
+      let model = this.store.createRecord( 'occasion', occasion );
+      this.store.push( 'occasion', model );
+    }
   }
 });
