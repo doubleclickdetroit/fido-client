a6dca059559ff4e355887b49b992d0d12bfafcd0
Ben Babics <bbabics@gmail.com>
2015-09-10 07:27:27 -0400

    relocated validations from controller to contact model

--- app/pods/contact/model.js
+++ app/pods/contact/model.js
@@ -1,10 +1,23 @@
 import DS from 'ember-data';
+import EmberValidations from 'ember-validations';
 
-export default DS.Model.extend({
+export default DS.Model.extend(EmberValidations.Mixin, {
   occasions: DS.hasMany( 'occasion' ),
 
   firstName   : DS.attr(),
   lastName    : DS.attr(),
   gender      : DS.attr( 'number', { defaultValue: 1 } ),
-  relationship: DS.attr( 'number', { defaultValue: 1 } )
+  relationship: DS.attr( 'number', { defaultValue: 1 } ),
+
+  validations: {
+    firstName: {
+      presence: true,
+      length: { minimum: 2 }
+    },
+
+    lastName: {
+      presence: true,
+      length: { minimum: 3 }
+    }
+  }
 });
