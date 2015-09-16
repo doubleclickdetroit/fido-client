5fdb976837e1a579d16b40d78d7adca84da4cbce
Ben Babics <bbabics@gmail.com>
2015-09-16 11:06:11 -0400

    update contentSecurityPolicy to allow requests to localhost:3000

--- config/environment.js
+++ config/environment.js
@@ -11,7 +11,7 @@ module.exports = function(environment) {
       'default-src': "'none'",
       'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
       'font-src': "'self'",
-      'connect-src': "'self'",
+      'connect-src': "'self' http://localhost:3000",
       'img-src': "'self'",
       'style-src': "'self' 'unsafe-inline'",
       'frame-src': ""
