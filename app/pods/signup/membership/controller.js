8089ddba6d35e2476f83370dc3f5532b1c19ae8f
Ben Babics <bbabics@gmail.com>
2015-09-17 00:50:40 -0400

    disableNext instead of showNext on signup.membership

--- app/pods/signup/membership/controller.js
+++ app/pods/signup/membership/controller.js
@@ -4,8 +4,8 @@ import RegistrationControllerMixin from '../../../mixins/signup/membership';
 export default Ember.Controller.extend(RegistrationControllerMixin, {
   authenticator: 'simple-auth-authenticator:devise',
 
-  isAuthenticatedDidChange: Ember.observer('session.isAuthenticated', 'wizardStep.showNext', function() {
+  isAuthenticatedDidChange: Ember.observer('session.isAuthenticated', 'wizardStep.disableNext', function() {
     let isAuthenticated = this.get( 'session.isAuthenticated' );
-    this.set( 'wizardStep.showNext', isAuthenticated );
+    this.set( 'wizardStep.disableNext', !isAuthenticated );
   })
 });
