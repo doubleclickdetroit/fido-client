0b516f0625272e931b4325211a95cf54e37de282
Ben Babics <bbabics@gmail.com>
2015-09-23 09:51:08 -0400

    Signup cleanup

--- app/pods/signup/membership/route.js
+++ app/pods/signup/membership/route.js
@@ -5,14 +5,6 @@ export default Ember.Route.extend(WizardStepSignupMixin, {
   actions: {
     sessionAuthenticationSucceeded() {
       // prevent ember-simple-auth config.routeAfterAuthentication redirecting to 'index'
-    },
-    prevRoute(route) {
-      console.log( 'membership prev' );
-      return true;
-    },
-    nextRoute(route) {
-      console.log( 'membership next' );
-      return true;
     }
   }
 });
