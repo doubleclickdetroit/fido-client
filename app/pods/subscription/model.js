f49eeb226d320dc3d996c400687a55d915e773e1
Ben Babics <bbabics@gmail.com>
2015-09-30 11:12:08 -0400

    Settings Billing & Invoices connect with API

--- app/pods/subscription/model.js
+++ app/pods/subscription/model.js
@@ -4,6 +4,5 @@ export default DS.Model.extend({
   stripeToken       : DS.attr( 'string' ),
   currentPeriodStart: DS.attr( 'date' ),
   currentPeriodEnd  : DS.attr( 'date' ),
-  nextBillingDate   : DS.attr( 'date' ),
   price             : DS.attr( 'number' )
 });
