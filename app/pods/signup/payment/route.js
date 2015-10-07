d89a2fbe56e23b35b11b525c237dba1095d5e177
Ben Babics <bbabics@gmail.com>
2015-10-07 09:23:00 -0400

    signup#payment uses $.post instead of subscription model

--- app/pods/signup/payment/route.js
+++ app/pods/signup/payment/route.js
@@ -1,23 +1,16 @@
 import Ember from 'ember';
+import ENV from 'fido/config/environment';
 import WizardStepSignupMixin from '../../../mixins/wizard-steps-signup';
 
 export default Ember.Route.extend(WizardStepSignupMixin, {
   actions: {
     handlePaymentToken(token) {
-      // create the subscription
-      let subscription = this.store.createRecord('subscription', { stripeToken: token });
+      let url  = `${ENV.API_URL}/subscription/`;
+      let data = { subscription: { stripe_token: token } };
 
-      // set hasSubscriptionErrors to false
-      this.set( 'controller.hasSubscriptionErrors', false );
-
-      // create subscription, then update controller payment has processed
-      subscription.save()
-        .then(function() {
-          this.set( 'controller.hasProcessedPayment', true );
-        }.bind(this))
-        .fail(function() {
-          this.set( 'controller.hasSubscriptionErrors', true );
-        }.bind(this));
+      Ember.$.post( url, data )
+        .then(() => this.set( 'controller.hasProcessedPayment', true ))
+        .fail(() => this.set( 'controller.hasSubscriptionErrors', true ));
     }
   }
 });
--- app/pods/subscription/model.js
+++ app/pods/subscription/model.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  stripeToken       : DS.attr( 'string' ),
   currentPeriodStart: DS.attr( 'date' ),
   currentPeriodEnd  : DS.attr( 'date' ),
   price             : DS.attr( 'number' )
